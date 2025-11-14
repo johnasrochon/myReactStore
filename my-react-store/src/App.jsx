97ca0314c7dbd7b2ac327d85c8d8af52b963df00
john rochon <johnasrochon@gmail.com>
2025-11-13 18:50:27 -0600

    assignment1

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <div className="app">
+    <Navbar />
+    <main className="main-content">
+      <h1>Welcome To My Online Store</h1>
+      <p>Discover amazing products for low prices</p>
+    </main>
+  </div>
+  )
+}
+
+export default App
---
+++ my-react-store/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+            <div className="footer-bottom">
+                <p>this belongs to John Rochon</p>
+            </div>
+            </div>
+        </footer>
+    )
+  }
+
+
+export default Footer
